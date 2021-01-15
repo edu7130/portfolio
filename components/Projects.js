b5ce6c45709a783ebe9e4e73426f812b07963321
Eduardo <Eduardo@EDUGAMES>
2021-01-14 22:10:59 -0300

    Borrado de compnentes innecesarios

--- components/Projects.js
+++ components/Projects.js
@@ -1,7 +1,4 @@
 import CardProject from "./Cards/CardProject";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import Prueba from "../pages/_prueba";
 
 export default function Projects(props) {
   function CreateCards() {
@@ -11,16 +8,6 @@ export default function Projects(props) {
     return cards;
   }
   const { description, projects } = props.content;
-
-  const settings = {
-    vertical: false,
-    dots: true,
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 1,
-    slidesToScroll: 3,
-    autoplay: true,
-  };
 
   return (
     <div id="projects">
