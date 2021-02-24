10f84f21c673daf8940ec0a50d52d89fac6f5d1a
Eduardo <Eduardo@EDUGAMES>
2021-02-24 09:22:56 -0300

    Arreglado el error de icono pestaña

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
           content="Eduardo Nicolas Weidenbacher Programador"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="../icon/favicon.png" type="image/png" />
+        <link rel="icon" href="../icon/favicon.ico"/>
         <link
           href="https://fonts.googleapis.com/css2?family=Lexend+Peta&display=swap"
           rel="stylesheet"
