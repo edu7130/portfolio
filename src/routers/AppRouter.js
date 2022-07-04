3e9ca64103ada827272dd35d492ec02ca0151039
Eduardo Weidenbacher <eduardo_nico_2013@outlook.com>
2022-07-04 09:43:23 -0300

    se borró la ruta /resume  en el AppRouter.js

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -20,8 +20,6 @@ export const AppRouter = () => {
                                 <Route path="/about" element={<About />} />
                                 <Route path="/projects" element={<Projects />} />
                                 <Route path="/contact" element={<Contact />} />
-                                {/* <Route path="/resume" element={<Resume />} /> */}
-
                                 <Route path="/*" element={<ErrorPage />} />
                             </Routes>
                         </Main>
