dc748b125c3681acf8d3c2e85d70edf22135a14d
Eduardo Weidenbacher <eduardo_nico_2013@outlook.com>
2022-08-19 16:06:50 -0300

    se quitó el fetch de los proyectos Se reemplazo por un lista estatica

--- src/api/projects.js
+++ src/api/projects.js
@@ -1,11 +1,58 @@
-import axioss from 'axios';
+// import axioss from 'axios';
 
-const axios = axioss.create({
-    baseURL: process.env.REACT_APP_URL_BACKEND,
-    timeout: 10000
-})
+// const axios = axioss.create({
+//     baseURL: process.env.REACT_APP_URL_BACKEND,
+//     timeout: 10000
+// })
 
 export const getProjects = async () => {
-    const response = await axios.get('/api/project');
-    return response.data;
+    // const response = await axios.get('/api/project');
+
+    return [
+        {
+            "id": "32445e5b-2174-46f2-9d47-9f66e12d22dd",
+            "name": "Ticket application Socket",
+            "description": "Ticket application Socket. Made with NodeJs, Express, Socket.io",
+            "urlRepo": "https://github.com/edu7130/ticket-socket.git",
+            "url": "https://ticket.edu-dev.ar",
+            "platform": "WEB",
+            "img": "https://res.cloudinary.com/dpgbofmgc/image/upload/v1649378751/wgccaktxxqmlg28862na.png",
+        },
+        {
+            "id": "335ed553-6647-471b-9cd6-72717a23ab9a",
+            "name": "Portfolio",
+            "description": "Personal portfolio made with React, Styled Components and deployed on Heroku",
+            "urlRepo": "https://github.com/edu7130/portfolio.git",
+            "url": "https://edu-dev.ar",
+            "platform": "WEB",
+            "img": "https://res.cloudinary.com/dpgbofmgc/image/upload/v1649378788/oaefs2gkruzqx4feckbp.png",
+        },
+        {
+            "id": "94383d32-730e-4a39-aaa7-acfaa623de09",
+            "name": "Rest Server",
+            "description": "Rest Server, Auth with google, JWT, Images of product and User profile. Made with NodeJs, Express, MongoDB",
+            "urlRepo": "https://github.com/edu7130/node-rest-server.git",
+            "url": "https://demorest.edu-dev.ar",
+            "platform": "WEB",
+            "img": "https://res.cloudinary.com/dpgbofmgc/image/upload/v1649378824/z831phfln4jdgxzk27nd.png",
+        },
+        {
+            "id": "aa77ed45-61bf-4dd8-90f5-cf23c82448de",
+            "name": "Dolarama",
+            "description": "Shows the value of the dollar in Argentina, made with Flutter",
+            "urlRepo": "https://github.com/edu7130/dolarama_arg.git",
+            "url": "https://play.google.com/store/apps/details?id=com.eduweidenbacher.dolarama_arg",
+            "platform": "MOBILE",
+            "img": "https://res.cloudinary.com/dpgbofmgc/image/upload/v1649378857/nbqek23zxjdspsxxjlbg.png",
+        },
+        {
+            "id": "ebcc251c-916a-44cc-be53-369f57956fc5",
+            "name": "Info Peliculas",
+            "description": "Search all the movies that exist, see their ratings and their actors, made with Flutter",
+            "urlRepo": "https://github.com/edu7130/info-peliculas.git",
+            "url": "https://play.google.com/store/apps/details?id=com.eduweidenbacher.infopeliculas",
+            "platform": "MOBILE",
+            "img": "https://res.cloudinary.com/dpgbofmgc/image/upload/v1649378875/ipusqq6tygibaol3fa5x.png",
+        }
+    ];
 }
